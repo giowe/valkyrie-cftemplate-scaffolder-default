04e99f2eca20bfc5c13932c929864b0bb65dbb0c
Matteo Carrara <matte.carra@gmail.com>
2018-03-18 18:03:23 +0100

    Handle waitFor and optional templates

--- scaffolder.js
+++ scaffolder.js
@@ -1,30 +1,85 @@
 const templates = require('./templates.json');
 const fs = require('fs');
-module.exports = {
-  create: (cloudFormation, config) => {
-    const { vpc, 'vpc-nat': vpcNat, lambda } = config;
 
-    const { enabled: vpcEnabled, type: vpcType, parameters: { ClassB } } = vpc;
-    const { enabled: vpcNatEnabled, type: vpcNatType, parameters: { ClassB } } = vpcNat;
+const toCloudFormationParameters = (configNode) => {
+  return Object.keys(configNode).map(ParameterKey => ({
+    ParameterKey,
+    ParameterValue: configNode[ParameterKey]
+  }));
+};
+
+module.exports = {
+  create: (cloudFormation, config, s3Bucket, s3ObjectVersion) => {
+    const { vpc, 'vpc-nat': vpcNat, 'security-groups': securityGroups, lambda, EnvironmentName, Description } = config;
+    const { enabled: vpcEnabled, type: vpcType, parameters: vpcParameters } = vpc;
+    const { enabled: vpcNatEnabled, type: vpcNatType, parameters: vpcNatParameters } = vpcNat;
+    const { enabled: securityGroupEnabled  } = securityGroups;
+    const { parameters: lambdaParameters } = lambda;
 
     if(vpcEnabled) {
       cloudFormation.createStack({
         StackName: vpcType,
         TemplateBody: fs.readFileSync(`vpc/${vpcType}.yaml`),
-        Parameters: [
-          {
-            ParameterKey: 'ClassB',
-            ParameterValue: ClassB
-          }
-        ]
+        Parameters: toCloudFormationParameters(vpcParameters)
+      });
+
+      if(vpcNatEnabled) {
+        cloudFormation.createStack({
+          StackName: vpcNatType,
+          TemplateBody: fs.readFileSync(`vpc-nat/${vpcNatType}.yaml`),
+          Parameters: toCloudFormationParameters(vpcNatParameters).push({ ParameterKey: 'ParentVPCStack', ParameterValue: vpcType })
+        });
+      }
+
+      if(securityGroupEnabled) {
+        cloudFormation.createStack({
+          StackName: 'lambda-security-group',
+          TemplateBody: fs.readFileSync(`security-groups/lambda-security-group.yaml`),
+          Parameters: [
+            {
+              ParameterKey: 'EnvironmentName',
+              ParameterValue: EnvironmentName
+            },
+            {
+              ParameterKey: 'ParentVPCStack',
+              ParameterValue: vpcType
+            }
+          ]
+        });
+      }
+
+      cloudFormation.createStack({
+        StackName: 'lambda',
+        TemplateBody: fs.readFileSync(`lambda/lambda.yaml`),
+        Parameters: toCloudFormationParameters(lambdaParameters)
+          .concat([
+            {
+              ParameterKey: 'EnvironmentName',
+              ParameterValue: EnvironmentName
+            },
+            {
+              ParameterKey: 'LambdaDescription',
+              ParameterValue: Description
+            },
+            {
+              ParameterKey: 'ParentVPCStack',
+              ParameterValue: vpcType
+            },
+            {
+              ParameterKey: 'S3Bucket',
+              ParameterValue: s3Bucket
+            },
+            {
+              ParameterKey: 'S3ObjectVersion',
+              ParameterValue: s3ObjectVersion
+            },
+            {
+              ParameterKey: 'ParentSecurityGroupStack',
+              ParameterValue: 'lambda-security-group'
+            }
+          ])
       })
-
-
     }
-
-
-
-
   },
   update: (cloudFormation, config) => {},
   delete: (cloudFormation, config) => {},
