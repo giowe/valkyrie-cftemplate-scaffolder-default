fc6078482695c4ccabc191838282a04e636e80a9
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-03-18 17:14:12 +0100

    wrk in progress

--- scaffolder.js
+++ scaffolder.js
@@ -2,27 +2,27 @@ const templates = require('./templates.json');
 const fs = require('fs');
 module.exports = {
   create: (cloudFormation, config) => {
-    const { vpc, 'vpc-nat': vpcNat, lambda } = config;
-
-    const { enabled: vpcEnabled, type: vpcType, parameters: { ClassB } } = vpc;
-    const { enabled: vpcNatEnabled, type: vpcNatType, parameters: { ClassB } } = vpcNat;
-
-    if(vpcEnabled) {
-      cloudFormation.createStack({
-        StackName: vpcType,
-        TemplateBody: fs.readFileSync(`vpc/${vpcType}.yaml`),
-        Parameters: [
-          {
-            ParameterKey: 'ClassB',
-            ParameterValue: ClassB
-          }
-        ]
-      })
-
-
-    }
-
-
+    // const { vpc, 'vpc-nat': vpcNat, lambda } = config;
+    //
+    // const { enabled: vpcEnabled, type: vpcType, parameters: { ClassB } } = vpc;
+    // const { enabled: vpcNatEnabled, type: vpcNatType, parameters: { ClassB } } = vpcNat;
+    //
+    // if(vpcEnabled) {
+    //   cloudFormation.createStack({
+    //     StackName: vpcType,
+    //     TemplateBody: fs.readFileSync(`vpc/${vpcType}.yaml`),
+    //     Parameters: [
+    //       {
+    //         ParameterKey: 'ClassB',
+    //         ParameterValue: ClassB
+    //       }
+    //     ]
+    //   })
+    //
+    //
+    // }
+    //
+    //
 
 
   },
